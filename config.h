af209aa0d0e64ea2abbfb2837c2a5c60ae0dac7c
lochyb <lochyb@users.noreply.github.com>
2023-04-21 23:13:00 +0900

    2023 NO LED SCROLL UPDATE

--- config.h
+++ config.h
@@ -35,13 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #ifdef RGBLIGHT_ENABLE
-
-    #undef RGBLED_NUM
-    #define RGBLIGHT_LAYERS
-    #define RGBLIGHT_MAX_LAYERS 8
     #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL 
-    #define RGBLED_NUM 27
     #define RGBLIGHT_LIMIT_VAL 120
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
